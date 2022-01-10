70a7c1ab2340005c9f2136044cfe69958cb5a076
Rob Hess <hessro@oregonstate.edu>
2022-01-10 15:35:56 -0800

    Add code from 1/10 lecture.

--- src/HandlingEventsApp.js
+++ src/HandlingEventsApp.js
@@ -1,13 +1,42 @@
 import React from 'react';
 
+class ToggleButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOn: true
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }))
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>{this.state.isOn ? "ON" : "OFF"}</button>
+  }
+}
+
 function App(props) {
   return (
     <div>
-      <button>This is a button</button>
+      <ToggleButton />
+      <button onClick={(e) => {
+        console.log("== The button was clicked, event:", e)
+      }}>
+        This is a button
+      </button>
       <a
-        href="https://web.engr.oregonstate.edu/~hessro/teaching/cs499-w21"
+        href="https://web.engr.oregonstate.edu/~hessro/teaching/cs499-w22"
         target="_blank"
         rel="noreferrer"
+        onClick={(e) => {
+          e.preventDefault()
+          console.log("== The link was clicked")
+        }}
       >
         This is a link
       </a>
